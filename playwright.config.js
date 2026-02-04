bb5b514f2a3edbdd4d7c848d8c5c26c239f9a09f
firmanfajar19 <firmanfajar19@gmail.com>
2026-02-04 11:40:38 +0700

    comment report portal

--- playwright.config.js
+++ playwright.config.js
@@ -7,21 +7,21 @@ import path from 'path';
 
 dotenv.config({ path: path.resolve(__dirname, '.env') });
 
-const RPConfig = {
-  apiKey: process.env.RP_API_KEY,
-  endpoint: process.env.RP_ENDPOINT,
-  project: process.env.RP_PROJECT,
-  launch: process.env.GITHUB_EVENT_NAME === 'pull_request'
-    ? `PR-${process.env.GITHUB_REF_NAME}`
-    : `PROD-${process.env.GITHUB_RUN_NUMBER}`,
-  description: `Run #${process.env.GITHUB_RUN_NUMBER}`,
-  attributes: [
-    { key: 'branch', value: process.env.GITHUB_REF_NAME },
-    { key: 'trigger', value: process.env.GITHUB_EVENT_NAME },
-    { key: 'platform', value: os.platform() },
-    { key: 'node', value: process.version },
-  ],
-};
+// const RPConfig = {
+//   apiKey: process.env.RP_API_KEY,
+//   endpoint: process.env.RP_ENDPOINT,
+//   project: process.env.RP_PROJECT,
+//   launch: process.env.GITHUB_EVENT_NAME === 'pull_request'
+//     ? `PR-${process.env.GITHUB_REF_NAME}`
+//     : `PROD-${process.env.GITHUB_RUN_NUMBER}`,
+//   description: `Run #${process.env.GITHUB_RUN_NUMBER}`,
+//   attributes: [
+//     { key: 'branch', value: process.env.GITHUB_REF_NAME },
+//     { key: 'trigger', value: process.env.GITHUB_EVENT_NAME },
+//     { key: 'platform', value: os.platform() },
+//     { key: 'node', value: process.version },
+//   ],
+// };
 
 export default defineConfig({
   fullyParallel: false,
@@ -42,7 +42,7 @@ export default defineConfig({
       },
     ],
     ['list'],
-    ['@reportportal/agent-js-playwright', RPConfig],
+    // ['@reportportal/agent-js-playwright', RPConfig],
     ['playwright-smart-reporter', {
       outputFile: 'smart-report.html',
       historyFile: 'test-history.json',
